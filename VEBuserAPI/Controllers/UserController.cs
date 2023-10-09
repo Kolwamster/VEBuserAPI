c0746791f98597a0fcfd0b5bee92ced54ea96f63
Kolwamster <nikolaypogudo@gmail.com>
2023-10-09 09:54:46 +0300

    implement pagination for GET, implement PUT, add roles return to GET(id)

--- VEBuserAPI/Controllers/UserController.cs
+++ VEBuserAPI/Controllers/UserController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace VEBuserAPI.Controllers
 {
@@ -7,23 +8,45 @@ namespace VEBuserAPI.Controllers
     public class UserController : ControllerBase, IDisposable
     {
         private UserContext _db;
+
         public UserController()
         {
             _db = new UserContext();
         }
 
         // GET: api/<UserController>
-        [HttpGet]
-        public IEnumerable<User> Get()
+        [HttpGet("{pageSize}-{pageIndex}")]
+        public IEnumerable<object> Get(int pageSize, int pageIndex)
         {
-            return _db.Users;
+            var users = _db.Users.Skip(pageSize * (pageIndex - 1)).Take(pageSize)
+                        .SelectMany(u => u.Roles, (u, r) =>
+                            new {
+                                Id = u.Id,
+                                Name = u.Name,
+                                Age = u.Age,
+                                Email = u.Email,
+                                Roles = u.Roles
+                            }
+                        )
+                        .ToList();
+            return users;
         }
 
         // GET api/<UserController>/5
         [HttpGet("{id}")]
-        public User Get(Guid id)
+        public object Get(Guid id)
         {
-            IQueryable<User> users = _db.Users.Where(obj => obj.Id == id);
+            var users = _db.Users
+                        .Where(u => u.Id == id)
+                        .SelectMany(u => u.Roles, (u, i) =>
+                            new {
+                                Name = u.Name,
+                                Age = u.Age,
+                                Email = u.Email,
+                                Roles = u.Roles
+                            }
+                        )
+                        .ToList();
             return users.First();
         }
 
@@ -36,15 +59,36 @@ namespace VEBuserAPI.Controllers
             user.Name = value.Name;
             user.Age = value.Age;
             user.Email = value.Email;
+
             _db.Users.Add(user);
             _db.SaveChanges();
         }
 
         // PUT api/<UserController>/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] User value)
+        public void Put(Guid id, [FromBody] User value)
         {
-            throw new NotImplementedException();
+            User user = new User();
+            user.Id = id;
+            user.Name = value.Name;
+            user.Age = value.Age;
+            user.Email = value.Email;
+            _db.Users.Update(user);
+            _db.SaveChanges();
+        }
+
+        // PUT api/<UserController>/AddRole/5
+        [HttpPatch("AddRole/{id}")]
+        public void Patch(Guid id, [FromBody] Role value) 
+        {
+            User? user = _db.Users.Find(id);
+            Role? role = _db.Roles.Find(value.Id);
+            if (user is not null && role is not null)
+            {
+                user.Roles.Add(role);
+                _db.Users.Update(user);
+                _db.SaveChanges();
+            }
         }
 
         // DELETE api/<UserController>/5
