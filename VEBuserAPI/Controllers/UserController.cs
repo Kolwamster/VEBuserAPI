454b9def9455a1145da9d4f82e7784744ed419d7
Kolwamster <nikolaypogudo@gmail.com>
2023-10-11 13:12:43 +0300

    add http codes

--- VEBuserAPI/Controllers/UserController.cs
+++ VEBuserAPI/Controllers/UserController.cs
@@ -42,34 +42,49 @@ namespace VEBuserAPI.Controllers
 
         // GET: api/<UserController>
         [HttpGet]
-        public IEnumerable<object> GetUsers(int pageSize, int pageIndex, string? userName = "", int? age = 0, string? email = "", string? roleName = "")
+        public IActionResult GetUsers(int pageSize, int pageIndex, string? userName = "", int? age = 0, string? email = "", string? roleName = "")
         {
+            if (pageSize <= 0 || pageIndex <= 0)
+            {
+                return BadRequest();
+            }
+
+            if (age < 0)
+            {
+                return BadRequest();
+            }
+
             var users = _db.Users
                         .Include(user => user.Roles)
                         .Where(u => string.IsNullOrEmpty(userName) || u.Name.Contains(userName))
                         .Where(u => string.IsNullOrEmpty(email) || u.Email.Contains(email))
                         .Where(u => age == 0 || u.Age == age)
                         .Where(u => string.IsNullOrEmpty(roleName) || u.Roles.Any(r => r.Name.Contains(roleName)));
+
+            if (!users.Any())
+            {
+                return NotFound();
+            }
                         
             var page = users.Skip(pageSize * (pageIndex - 1))
                         .Take(pageSize)
                         .ToList();
-            return page;
+            return Ok(page);
         }
 
         // GET api/<UserController>/5
         [HttpGet("{id}")]
-        public object GetUser(Guid id)
+        public IActionResult GetUser(Guid id)
         {
             var users = _db.Users
                         .Include(user => user.Roles)
                         .Where(u => u.Id == id);
-            return users.First();
+            return users.Any() ? Ok(users.First()) : NotFound();
         }
 
         // POST api/<UserController>
         [HttpPost]
-        public void PostUser([FromBody] User value)
+        public IActionResult PostUser([FromBody] User value)
         {
             try
             {
@@ -78,24 +93,17 @@ namespace VEBuserAPI.Controllers
                 value.Id = Guid.NewGuid();
                 _db.Users.Add(value);
                 _db.SaveChanges();
-            }
-            catch (ArgumentNullException)
-            {
-
-            }
-            catch (ArgumentOutOfRangeException)
-            {
-
+                return Ok(value);
             }
             catch (Exception)
             {
-
+                return BadRequest();
             }
         }
 
         // PUT api/<UserController>/5
         [HttpPut("{id}")]
-        public void PutUser(Guid id, [FromBody] User value)
+        public ActionResult PutUser(Guid id, [FromBody] User value)
         {
             try
             {
@@ -103,43 +111,56 @@ namespace VEBuserAPI.Controllers
                 value.Id = id;
                 _db.Users.Update(value);
                 _db.SaveChanges();
-            }
-            catch (ArgumentNullException)
-            {
-
-            }
-            catch (ArgumentOutOfRangeException)
-            {
-
+                return Ok(value);
             }
             catch (Exception)
             {
-
+                return BadRequest();
             }
         }
 
         // PUT api/<UserController>/AddRole/5
         [HttpPatch("AddRole/{id}")]
-        public void AddUserRole(Guid id, [FromBody] Role value) 
+        public IActionResult AddUserRole(Guid id, [FromBody] Role value) 
         {
             User? user = _db.Users.Find(id);
             Role? role = _db.Roles.Find(value.Id);
             if (user is not null && role is not null)
             {
-                user.Roles.Add(role);
-                _db.Users.Update(user);
-                _db.SaveChanges();
+                try
+                {
+                    user.Roles.Add(role);
+                    _db.Users.Update(user);
+                    _db.SaveChanges();
+                    return Ok(user);
+                }
+                catch (Exception)
+                {
+                    return BadRequest();
+                }
+            }
+            else
+            {
+                return NotFound();
             }
         }
 
         // DELETE api/<UserController>/5
         [HttpDelete("{id}")]
-        public void DeleteUser(Guid id)
+        public IActionResult DeleteUser(Guid id)
         {
-            User user = new User();
-            user.Id = id;
-            _db.Remove(user);
-            _db.SaveChanges();
+            try
+            {
+                User user = new User();
+                user.Id = id;
+                _db.Remove(user);
+                _db.SaveChanges();
+                return Ok();
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
         }
 
         public void Dispose()
